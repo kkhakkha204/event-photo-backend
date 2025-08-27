9e5beecaae87b205c10ab412322c17a429f7d45b
khacl <kkhkkha204@gmail.com>
2025-08-27 13:35:23 +0700

    reset 1

--- models.py
+++ models.py
@@ -3,16 +3,16 @@ from sqlalchemy.sql import func
 from database import Base
 
 class Image(Base):
-    tablename = "images"
-
+    __tablename__ = "images"
+    
     id = Column(Integer, primary_key=True, index=True)
     url = Column(String, nullable=False)
     event_id = Column(Integer, nullable=True)
     uploaded_at = Column(DateTime(timezone=True), server_default=func.now())
 
 class FaceEmbedding(Base):
-    tablename = "face_embeddings"
-
+    __tablename__ = "face_embeddings"
+    
     id = Column(Integer, primary_key=True, index=True)
     image_id = Column(Integer, ForeignKey("images.id"))
     embedding = Column(JSON, nullable=False)
